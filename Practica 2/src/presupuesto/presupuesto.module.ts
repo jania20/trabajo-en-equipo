2d4f72a216974cd80d0e53cd9da8500187332691
jania20 <141696293+jania20@users.noreply.github.com>
2023-08-25 11:07:23 -0600

    presupuesto.module.ts

---
+++ Practica 2/src/presupuesto/presupuesto.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { PresupuestoPageRoutingModule } from './presupuesto-routing.module';
+
+import { PresupuestoPage } from './presupuesto.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    PresupuestoPageRoutingModule
+  ],
+  declarations: [PresupuestoPage]
+})
+export class PresupuestoPageModule {}
+ 56 changes: 56 additions & 0 deletions56  
