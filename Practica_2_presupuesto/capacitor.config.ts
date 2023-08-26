f16fee395dda9be951ab56513ffd550a7e4096b9
Jania Moreno <janiamsanchez@gmail.com>
2023-08-26 12:24:43 -0600

    Practica 2-presupuesto

---
+++ Practica_2_presupuesto/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Practica_2_presupuesto',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
